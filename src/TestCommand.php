7624b86930769477e4cf3850877372d06ef12187
Pablo <antonenkopablo@gmail.com>
2019-04-16 15:34:05 +0300

    example

--- src/TestCommand.php
+++ src/TestCommand.php
@@ -2,8 +2,8 @@
 require_once 'DataBaseManager.php';
 
 $dataBaseManager = new DataBaseManager();
-$data = $dataBaseManager->select('name')->from('user')->order_by('u.id', 'DESC');//->getResult();
-
+$data = $dataBaseManager->select('name')->from('user', 'u')->order_by('u.id');//->getResult();
+var_dump($data);
 
 
 
