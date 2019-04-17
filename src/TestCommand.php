f276b8fab2193db741592be8db1706a9c6fd7eab
Pablo <antonenkopablo@gmail.com>
2019-04-17 11:41:02 +0300

    example

--- src/TestCommand.php
+++ src/TestCommand.php
@@ -2,7 +2,9 @@
 require_once 'DataBaseManager.php';
 
 $dataBaseManager = new DataBaseManager();
-$data = $dataBaseManager->select('name')->from('user', 'u')->order_by('u.id');//->getResult();
+//$data = $dataBaseManager->select('name')->from('user', 'u')->order_by('u.id');//->getResult();
+$data = $dataBaseManager->insert('user', 'id, name')->values("8, 'new_user'")->exec();
+
 var_dump($data);
 
 
