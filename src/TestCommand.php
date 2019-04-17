f73c7ee07c897cc81982e0f48d17951177b7f53b
Pablo <antonenkopablo@gmail.com>
2019-04-17 17:57:59 +0300

    help me. method getQuery does not work.something with data types

--- src/TestCommand.php
+++ src/TestCommand.php
@@ -6,7 +6,9 @@ $dataBaseManager = new DataBaseManager();
 //$data = $dataBaseManager->insert('user')->values("14, 'nub'")->exec();
 //$data = $dataBaseManager->update('user')->set("name = 'wtf2'")->exec();
 //$data = $dataBaseManager->update('user')->set("name = 'wtf3'")->orWhere('id = 4', 'id = 3')->exec();
-$data = $dataBaseManager->update('user')->set("name = 'shit'")->inWhere('id', '0, 1, 2' )->exec();
+//$data = $dataBaseManager->update('user')->set("name = 'shit'")->inWhere('id', '0, 1, 2' )->exec();
+
+$data = $dataBaseManager->select('u')->from('user', 'u')->order_by('u.id')->where('id>0')->andWhere('name = NOT NULL')->getResult();
 
 var_dump($data);
 
