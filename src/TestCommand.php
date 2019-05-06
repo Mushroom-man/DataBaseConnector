76b140598d0fb66afddcb5cc135a986abcad50ba
Pablo <antonenkopablo@gmail.com>
2019-05-06 21:35:12 +0300

    example methods prepare,execute,andWhere

--- src/TestCommand.php
+++ src/TestCommand.php
@@ -8,10 +8,6 @@ $dataBaseManager = new DataBaseManager();
 //$data = $dataBaseManager->update('user')->set("name = 'wtf3'")->orWhere('id = 4', 'id = 3')->exec();
 //$data = $dataBaseManager->update('user')->set("name = 'shit'")->inWhere('id', '0, 1, 2' )->exec();
 
-$data = $dataBaseManager->select('*')->andWhere('name = bla')->order_by('u.id', 'DESC')->where('name = firstuser')->from('user', 'u')->andWhere('name = NOT NULL')->getQuery();//->getResult();
+$data = $dataBaseManager->select('*')->andWhere("id >= 7")->order_by('u.id', 'DESC')->from('user', 'u')->andWhere("name = 'wtf2'")->prepare()->execute();//->getResult();
 
 var_dump($data);
-
-
-
-
