c28834a1de39c691a064a7357434ff4a0f563e8a
Pablo <antonenkopablo@gmail.com>
2019-05-14 13:39:27 +0300

    construct calls the parser

--- src/DataBaseManager.php
+++ src/DataBaseManager.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'ConfigParser.php';
 
 class DataBaseManager
 {
@@ -64,13 +65,16 @@ class DataBaseManager
     private $requestType;
 
     /**
-     * DataBase constructor.
+     * DataBaseManager constructor.
      */
     public function __construct()
     {
-        $dsn = "mysql:host=127.0.0.1;dbname=MyTest;charset=utf8";
-        $user = 'root';
-        $pass = 'LizardKing7';
+        $className = 'ConfigParser';
+        $className::parsingData();
+        $dsn = "mysql:host=" . ConfigParser::$parsedData["db_host:"] . ";dbname=" . ConfigParser::$parsedData["db_name:"] . ";charset=" . ConfigParser::$parsedData["charset:"];
+        //$dsn = "mysql:host=127.0.0.1;dbname=MyTest;charset=utf8";
+        $user = ConfigParser::$parsedData["db_user:"];
+        $pass = ConfigParser::$parsedData["db_password:"];
         $this->connection = new PDO($dsn, $user, $pass);
     }
 
