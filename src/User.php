8ed865b02b9d45508f471ed2723ecdfa728e0af1
Pablo <antonenkopablo@gmail.com>
2019-05-17 17:01:47 +0300

    added getters and setters

--- src/User.php
+++ src/User.php
@@ -3,12 +3,79 @@
 
 class User
 {
+    /** @var string */
     public $entityName;
 
+    /** @var string */
     public $table = 'user';
 
+    /** @var integer */
     public $id;
 
+    /** @var string */
     public $name;
 
+    /**
+     * @return mixed
+     */
+    public function getEntityName()
+    {
+        return $this->entityName;
+    }
+
+    /**
+     * @param mixed $entityName
+     */
+    public function setEntityName($entityName)
+    {
+        $this->entityName = $entityName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTable()
+    {
+        return $this->table;
+    }
+
+    /**
+     * @param string $table
+     */
+    public function setTable($table)
+    {
+        $this->table = $table;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
 }
