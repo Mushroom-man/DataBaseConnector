2feda7b9a66dd386b76c807449d4d642764f2d87
Pablo <antonenkopablo@gmail.com>
2019-05-17 20:29:53 +0300

    +common methods and properties

--- src/BaseEntity.php
+++ src/BaseEntity.php
@@ -3,5 +3,41 @@
 
 class BaseEntity
 {
+    /** @var integer */
+    private $id;
 
+    /** @var string */
+    private $name;
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param integer $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
 }
