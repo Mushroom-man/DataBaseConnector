896a27e0684c74bf43cce5280fba40f77b7e5c1a
Pablo <antonenkopablo@gmail.com>
2019-05-17 11:08:27 +0300

    added checking for properties

--- src/EntityManager.php
+++ src/EntityManager.php
@@ -9,11 +9,17 @@ class EntityManager
 
     private $oneEntity;
 
+    private $desiredId;
+
+    private $arrObjVars;
+
     public function __construct($className)
     {
       $this->dbConnect = new DataBaseManager();
 
       $this->oneEntity = new $className;
+
+      $this->getProperties();
     }
 
     public function setEntityName($entityName)
@@ -25,18 +31,39 @@ class EntityManager
 
     public function findById($desiredId)
     {
+        $this->desiredId = $desiredId;
+
         $queryResult = $this->dbConnect->select('*')->from($this->oneEntity->table)->where('id = ' . $desiredId)->getQuery()->prepare()->execute();
 
-        $this->setProperties($queryResult);
+        if ($queryResult == []) {
+            try {
+                throw new Exception("Does not exist in the database");
+            } catch(Exception $e) {
+                echo $e->getMessage();
+            }
+        } else {
+            $this->setProperties($queryResult);
+        }
+
+        return $this;
+    }
+
+    public function getProperties()
+    {
+        $this->arrObjVars = get_object_vars($this->oneEntity);
 
         return $this;
     }
 
     public function setProperties($queryResult)
     {
-        $this->oneEntity->id = $queryResult[0]["id"];
-
-        $this->oneEntity->name = $queryResult[0]["name"];
+        if (array_key_exists("id", $this->arrObjVars)) {
+            $this->oneEntity->id = $queryResult[0]["id"];
+        }
+        if (array_key_exists("name", $this->arrObjVars) && $queryResult[0]["name"] !== NULL) {
+            $this->oneEntity->name = $queryResult[0]["name"];
+        }
+        $this->dbConnect = new DataBaseManager();
 
         return $this;
     }
