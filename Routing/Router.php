67284eada9152803863f94f41bc153a87d086367
Pablo <antonenkopablo@gmail.com>
2019-06-11 19:47:03 +0300

    slightly corrected

--- Routing/Router.php
+++ Routing/Router.php
@@ -9,40 +9,45 @@ class Router
 
     public $parsedConfig = [];
 
-    public function __construct($arr)
+    public $configName;
+
+    public function __construct($incomingRequest)
     {
-        $arr = array_flip($arr);
-
-        $this->requestData = $arr;
-
-        $this->url = array_pop($arr);
+        $this->requestData = $incomingRequest;
 
         $this->parseControllerConfig();
 
-        $this->createRout($this->url);
+        $this->handleRequest($incomingRequest);
+    }
+
+    private function handleRequest($incomingRequest)
+    {
+        $incomingRequest = array_flip($incomingRequest);
+        $this->url = array_pop($incomingRequest);
+        $urlElements = explode('/', $this->url);
+        $requestProcessingOptions = [];
+
+        foreach ($this->parsedConfig as $configParams) {
+           if (preg_match($configParams["path"], $this->url) == 1) {
+               $requestProcessingOptions['controllerName'] = $configParams["controller"];
+               $requestProcessingOptions['controllerMethod'] = $configParams["controllerMethod"];
+           }
+        }
+
+        return $requestProcessingOptions;
     }
 
     private function parseControllerConfig()
     {
-        $parsConfig = file('/var/www/html/DataBaseConnector/Routing/ConfigControllers.yml', FILE_IGNORE_NEW_LINES);
+        $parsConfig = file('/var/www/html/DataBaseConnector/Routing/ConfigControllers.yml', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
         foreach ($parsConfig as $key => $value) {
-            $parsElement = explode(': ', $value);
-            $this->parsedConfig[] = $parsElement;
-        }
-
-        return $this;
-    }
-
-    private function createRout($url)
-    {
-        for ($k = 1; $k < count($this->parsedConfig); $k = $k + 5) {
-            $matchingResult = preg_match($this->parsedConfig[$k][1], $url);
-            if ($matchingResult == 1 && $this->parsedConfig[$k+1][1] == 'GET') {
-                $controller = $this->parsedConfig[$k + 2][1] . '.php';
-                var_dump($controller);
-                $methodController = $this->parsedConfig[$k + 3][1];
-                var_dump($methodController);
+            $configElement = explode(': ', $value);
+            if(count($configElement) == 1) {
+                $this->configName = trim($configElement[0], ":");
+                $this->parsedConfig[$this->configName] = [];
+            } else {
+                $this->parsedConfig[$this->configName][trim($configElement[0])] = trim($configElement[1]);
             }
         }
 
